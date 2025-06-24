99918e32c118d18cbb82d485787d942897c7af4d
bolivkazeleyau <didlikeshow@gmail.com>
2025-06-24 04:00:16 +0300

    Icons test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,24 @@ import { ScrollRestorationHandler } from "@/components/ScrollRestorationHandler"
 import { ReactLenis } from "@/lib/lenis"
 import { ThemeMetaUpdater } from "@/components/theme_management/ThemeMetaUpdater";
 import { VideoProvider } from "@/contexts/VideoContext";
+import { Metadata } from "next";
 
 const pretendard = localFont({
   src: "../fonts/Pretendard/variable/woff2/PretendardStdVariable.woff2",
   variable: '--font-pretendard',
   display: 'swap',
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [
+      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" }
+    ],
+    apple: "/icon-192x192.png",
+  },
+};
 
 export default function RootLayout({
   children,
